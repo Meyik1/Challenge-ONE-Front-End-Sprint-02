a16d0f0b77df7e983574989b68b9190c29bd5d89
Manuel <meeyik.22@gmail.com>
2023-07-20 21:25:21 -0600

    agregando validación para ingresar al modo administrador

--- Js/Validaciones.js
+++ Js/Validaciones.js
@@ -1,1 +1,16 @@
-console.log("validaciones");
+const correocorrecto = document.getElementById("correo");
+const passcorrecto = document.getElementById("pass");
+const enviarBtn = document.getElementById("enviar");
+const form = document.getElementById("form");
+
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    if(correocorrecto.value == "meeyik.22@gmail.com" && passcorrecto.value == "alura123"){
+        console.log(correocorrecto);
+        enviarBtn.enable = true;
+        window.location.href = "/Pantallas/Modo_administrador.html";
+    }else{
+        enviarBtn.disable = false;
+        alert("El correo o la contraseña ingresada es incorrecto")
+    }
+});
